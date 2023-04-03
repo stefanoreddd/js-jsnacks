7d907a7c5d67f10942e9d9f8a7f9f672ebd86821
stefanoreddd <stefanogg95@gmail.com>
2023-04-03 20:01:07 +0200

    jsnack7 fixed.

--- jsnack7/main.js
+++ jsnack7/main.js
@@ -1,8 +1,10 @@
-let peppino = prompt("Inserisci un numero");
+let peppino = prompt("Inserisci un numero da 1 a 1000");
 peppino = parseInt(peppino);
 
 for (let i=2; i<=peppino; i=i+2){
-    console.log(i);
+        if (peppino <= 1000){
+            console.log(i);
+        } else{
+        console.log("Il numero deve essere inferiore a 1000!");
+    }
 }
-
-
